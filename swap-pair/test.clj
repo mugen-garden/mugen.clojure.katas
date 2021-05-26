84017316ea4a6a1c79f9e6af5b5dc1da1222ae58
nanoni17728 <nanoni@riseup.net>
2021-05-26 10:53:44 +0200

    Forgot to add the expected output of the test

--- swap-pair/test.clj
+++ swap-pair/test.clj
@@ -1,5 +1,5 @@
 (ns kata.swap-pair.test)
 
-(def test-1 "eHll oW rodl")
-(def test-2 "24609")
-(def test-3 "F")
+(def test-1 ["eHll ooWlr!d" "Hello World!"])
+(def test-2 ["24609" "42069"])
+(def test-3 ["F" "F"])
